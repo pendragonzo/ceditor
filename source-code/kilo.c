3c2d9f1171c966ca70a8ea8e938301d51c8c2b55
pendragonzo <hi@pendragonzo.org>
2020-10-08 11:38:48 +0100

    error handling

--- source-code/kilo.c
+++ source-code/kilo.c
@@ -6,6 +6,11 @@
 
 struct termios orig_termios;
 
+void die(const char *s) {
+	perror(s);
+	exit(1);
+}
+
 void disableRawMode() {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
@@ -15,7 +20,12 @@ void enableRawMode() {
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -23,13 +33,15 @@ void enableRawMode() {
 int main() {
 	enableRawMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	while (1) {
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
 		if (iscntrl(c)) {
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		} else {
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q') break;
 	}
 
 	return 0;
